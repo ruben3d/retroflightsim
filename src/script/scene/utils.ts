a4844fe956f7f791cf4caafb7985fae22af23b5a
Ruben Moreno Montoliu <ruben3d@gmail.com>
2022-01-31 19:30:33 +0000

    Fix fog distance for shaded models

--- src/script/scene/utils.ts
+++ src/script/scene/utils.ts
@@ -1,19 +1,23 @@
 import * as THREE from 'three';
 import { SceneMaterialUniforms } from './materials/materials';
 
+const camDir = new THREE.Vector3();
+const camPos = new THREE.Vector3();
+
 export function updateUniforms(this: THREE.Mesh, renderer: THREE.WebGLRenderer, scene: THREE.Scene, camera: THREE.Camera, geometry: THREE.BufferGeometry, material: THREE.Material, group: THREE.Group) {
     const m = (material as THREE.ShaderMaterial);
     const u = m.uniforms as SceneMaterialUniforms;
 
+    camera.getWorldDirection(camDir).setY(0.0).normalize();
+    camera.getWorldPosition(camPos);
+    const camD = camPos.negate().dot(camDir);
+
     if (m.userData?.shaded) {
-        const matrix = u.normalModelMatrix.value as THREE.Matrix3;
-        matrix.getNormalMatrix(this.matrixWorld);
-        u.distance.value = this.position.clone().setY(0.0).distanceTo(camera.position.clone().setY(0.0));
+        (u.normalModelMatrix.value as THREE.Matrix3).getNormalMatrix(this.matrixWorld);
+        u.distance.value = this.position.dot(camDir) + camD;
     } else {
-        const n = u.vCameraNormal.value as THREE.Vector3;
-        camera.getWorldDirection(n).setY(0.0).normalize();
-        const p = camera.getWorldPosition(new THREE.Vector3());
-        u.vCameraD.value = p.multiplyScalar(-1).dot(n);
+        (u.vCameraNormal.value as THREE.Vector3).copy(camDir);
+        u.vCameraD.value = camD;
     }
     m.uniformsNeedUpdate = true;
 }
